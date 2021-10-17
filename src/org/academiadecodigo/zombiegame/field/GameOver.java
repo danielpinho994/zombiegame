590dfa5516490c247f9e1754120dccad511c82ad
David Miranda <davidcv401@hotmail.com>
2021-10-17 19:51:52 +0100

    restart game

--- src/org/academiadecodigo/zombiegame/field/GameOver.java
+++ src/org/academiadecodigo/zombiegame/field/GameOver.java
@@ -16,13 +16,17 @@ public class GameOver {
     private int backgroundMiddleRow = Background.getRows()/2;
 
 
-    public GameOver(){
+    public GameOver() throws InterruptedException {
 
         //getGameOverBackground();
         gameOverTextPos = new Position(backgroundMiddleCol-330,backgroundMiddleRow-200);
         gameOverText = new Picture(gameOverTextPos.getCol()*Background.getCellSize(), gameOverTextPos.getRow()* Background.getCellSize(),"assets/GameOverZombies-removebg-preview.png");
         gameOverText.grow(-2,-1);
         gameOverText.draw();
+        for(int i = 0; i < 100; i++){
+            Thread.sleep(20);
+        }
+        gameOverText.delete();
     }
 
     /*private void getGameOverBackground(){
